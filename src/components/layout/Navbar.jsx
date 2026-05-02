feb8b6400c6fbf959522a9c4b86109f4ea1a35e1
arjunsan22 <arjunanil2114@gmail.com>
2026-05-02 18:38:59 +0530

    updated project icon

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <Link to="/" className="text-2xl font-bold text-blue-600">
-            FindMyWork
+            🇫​🇮​🇳​🇩​🇲​🇾​🇼​🇴​🇷​🇰
           </Link>
 
 
